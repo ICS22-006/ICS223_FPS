8239e6217ed907f0a8ffa08273afdad1cc48b998
ICS22-006 <113048821+ICS22-006@users.noreply.github.com>
2023-03-09 11:14:21 -0800

    Lab 7 Done

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour
 {
+    // Multiple enemies
     [SerializeField] private GameObject enemyPrefab;
-    private Vector3 spawnPoint = new Vector3(0, 0, 5);
+    private Vector3 enemySpawnPoint = new Vector3(0, 0, 5);
     private int numOfEnemies;
     private GameObject[] enemies;
+
+    // Multiple iguanas
+    [SerializeField] private GameObject iguanaPrefab;
+    private Vector3 iguanaSpawnPoint = new Vector3(5, 0, 5);
+    private int numOfIguanas;
+    private GameObject[] iguanas;
 
     private void Start()
     {
         numOfEnemies = 5;
         enemies = new GameObject[numOfEnemies];
+
+        numOfIguanas = 10;
+        iguanas = new GameObject[numOfIguanas];
+        for (int i = 0; i < numOfIguanas; i++)
+        {
+            iguanas[i] = Instantiate(iguanaPrefab) as GameObject;
+            iguanas[i].transform.position = iguanaSpawnPoint;
+            float angle = Random.Range(0, 360);
+            iguanas[i].transform.Rotate(0, angle, 0);
+        }
     }
 
     // Update is called once per frame
@@ -24,7 +41,7 @@ public class SceneController : MonoBehaviour
             if (enemies[i] == null)
             {
                 enemies[i] = Instantiate(enemyPrefab) as GameObject;
-                enemies[i].transform.position = spawnPoint;
+                enemies[i].transform.position = enemySpawnPoint;
                 float angle = Random.Range(0, 360);
                 enemies[i].transform.Rotate(0, angle, 0);
             }
